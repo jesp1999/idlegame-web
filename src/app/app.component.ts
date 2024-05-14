83fd49a4b45922a84435ec251c14cd9e82bdd823
Caleb-Littlejohn <43481221+Caleb-Littlejohn@users.noreply.github.com>
2024-05-14 15:49:47 -0700

    Added Google sign in and added css (#3)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import {CommonModule} from "@angular/common";
 import {RouterOutlet} from "@angular/router";
 import {GameComponent} from "./features/game/game.component";
 import {ThemeToggleComponent} from "./shared/theme-toggle/theme-toggle.component";
+import {LoginComponent} from "./features/login/login.component";
 
 @Component({
   selector: 'idle-root',
   standalone: true,
-  imports: [GameComponent, CommonModule, RouterOutlet, ThemeToggleComponent],
+  imports: [GameComponent, CommonModule, RouterOutlet, ThemeToggleComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
